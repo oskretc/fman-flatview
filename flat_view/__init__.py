2b947b446dac5ad88fe54e981282ec7469004c5b
oskretc <otorresbaca@gmail.com>
2018-04-11 14:57:41 +0200

    Working some how recursively

--- flat_view/__init__.py
+++ flat_view/__init__.py
@@ -14,7 +14,6 @@ class FlatView(DirectoryPaneCommand):
 
 from fman.fs import FileSystem, exists
 from os import listdir, path
-from os import path as ppp
 from fman.url import dirname, as_human_readable
 
 
@@ -22,24 +21,9 @@ class Example(FileSystem):
 
 	scheme = 'example://'
 
-	# def __init__(self, path):
-	# 	self.current_dir= path
-	# 	show_status_message('this is my paht' + dirname(url))
-
-	# TODO: implement columns
 	def get_default_columns(self, path):
-		# return 'core.Name', 'core.Size', 'core.Modified'
 		return 'core.Name', 'flat_view.FullPath'
-	# def size_bytes(self, path):
-	# 	if path == 'File.txt':
-	# 		return 4 * 1024
-	# 	elif path == 'Image.jpg':
-	# 		return 1300 * 1024
-	# def modified_datetime(self, path):
-	# 	if path == 'File.txt':
-	# 		return datetime(2018, 2, 1, 12, 7)
-	# 	elif path == 'Image.jpg':
-	# 		return datetime(2017, 4, 15, 9, 36)		
+	
 
 	def iterdir(self, path):
 		
@@ -47,21 +31,41 @@ class Example(FileSystem):
 		baseurl = 'file://' + path
 		pathhuman = as_human_readable(baseurl)
 
-		list_directory_content = listdir(pathhuman)
+		# list_directory_content = listdir(pathhuman)
+		# 
 		
+		return self.find_files(baseurl)		
 		
-		for file_name in list_directory_content:
-			completeurl=join(baseurl, file_name)
-			# f= open('c:\\tmp\\flat.txt', 'a')
-			# if is_dir(join(baseurl, file_name)):
-			# 	f.write(join('\n' +baseurl, file_name))
-			# else:
-			# 	f.write(join('\n not dir' +baseurl, file_name))
+		# for file_name in list_directory_content:
+		# 	completeurl=join(baseurl, file_name)
+		# 	# f= open('c:\\tmp\\flat.txt', 'a')
+		# 	# if is_dir(join(baseurl, file_name)):
+		# 	# 	f.write(join('\n' +baseurl, file_name))
+		# 	# else:
+		# 	# 	f.write(join('\n not dir' +baseurl, file_name))
 
-			# f.close()			
-			if not is_dir(completeurl):
-				# yield pathhuman + '\\' + file_name
-				yield file_name
+		# 	# f.close()			
+		# 	if not is_dir(completeurl):
+		# 		# yield pathhuman + '\\' + file_name
+		# 		yield file_name
+
+
+	def find_files(self, path):
+		f= open('c:\\tmp\\flat.txt', 'a')
+		# f.write('\n' + path)
+		# f.close()
+		list_files=[]
+		for file_name in listdir(as_human_readable(path)):
+			file_path=join(path, file_name)
+			list_files.append(file_path)
+			f.write('\n' + as_human_readable(file_path))
+			if is_dir(file_path):
+				list_files+=self.find_files(file_path)
+		f.close()
+		return list_files
+
+
+
 
 
 	def is_dir(self, path):
@@ -81,4 +85,4 @@ from fman.fs import Column
 class FullPath(Column):
 	"""Returns the full path of a url"""
 	def get_str(self, url):			
-		return as_human_readable(url.replace('example://', 'file://'))
+		return (url)
