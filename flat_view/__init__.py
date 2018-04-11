d4e0fee75a5758932d87a353220e4c1673fba4e4
oskretc <otorresbaca@gmail.com>
2018-04-11 15:49:58 +0200

    Now able to display all files correctly, but the full path is not correct

--- flat_view/__init__.py
+++ flat_view/__init__.py
@@ -1,8 +1,7 @@
+from os import listdir
 from fman import DirectoryPaneCommand, show_alert, show_status_message
-from fman.url import dirname, as_human_readable, as_url, join
-
-
-# from .Example import *
+from fman.url import dirname, as_human_readable, join, splitscheme
+from fman.fs import FileSystem, Column
 
 class FlatView(DirectoryPaneCommand):
 	def __call__(self, url=None):
@@ -11,11 +10,6 @@ class FlatView(DirectoryPaneCommand):
 	
 		self.pane.set_path('example://'+ dirname(url).replace('file://', ''))
 		# show_alert(self.pane.get_path())
-
-from fman.fs import FileSystem, exists
-from os import listdir, path
-from fman.url import dirname, as_human_readable
-
 
 class Example(FileSystem):
 
@@ -26,63 +20,36 @@ class Example(FileSystem):
 	
 
 	def iterdir(self, path):
-		
 	
 		baseurl = 'file://' + path
-		pathhuman = as_human_readable(baseurl)
 
-		# list_directory_content = listdir(pathhuman)
-		# 
-		
 		return self.find_files(baseurl)		
-		
-		# for file_name in list_directory_content:
-		# 	completeurl=join(baseurl, file_name)
-		# 	# f= open('c:\\tmp\\flat.txt', 'a')
-		# 	# if is_dir(join(baseurl, file_name)):
-		# 	# 	f.write(join('\n' +baseurl, file_name))
-		# 	# else:
-		# 	# 	f.write(join('\n not dir' +baseurl, file_name))
 
-		# 	# f.close()			
-		# 	if not is_dir(completeurl):
-		# 		# yield pathhuman + '\\' + file_name
-		# 		yield file_name
+
 
 
 	def find_files(self, path):
-		f= open('c:\\tmp\\flat.txt', 'a')
-		# f.write('\n' + path)
-		# f.close()
+
 		list_files=[]
 		for file_name in listdir(as_human_readable(path)):
 			file_path=join(path, file_name)
-			list_files.append(file_path)
-			f.write('\n' + as_human_readable(file_path))
-			if is_dir(file_path):
-				list_files+=self.find_files(file_path)
-		f.close()
+			scheme, simplepath =splitscheme(file_path)
+			# Remove folders and files starting with .   TODO: improve this
+			if not file_name.startswith('.'):				
+				if is_dir(file_path):
+					list_files+=self.find_files(file_path)
+				# Only add Files not Directories
+				else:
+					list_files.append(simplepath)
+
 		return list_files
-
-
-
-
-
-	def is_dir(self, path):
-		# f= open('c:\\tmp\\flat.txt', 'a')
-		# f.write('\n' + path)
-		# f.close()
-		# if ppp.isdir(path):
-		# 	return path
-		# else:
-		return path == 'as'		
 
 	def full_path(self, path):
 		return path
 
-from fman.fs import Column
 
 class FullPath(Column):
 	"""Returns the full path of a url"""
 	def get_str(self, url):			
-		return (url)
+		scheme, path =splitscheme(url)
+		return (path)
